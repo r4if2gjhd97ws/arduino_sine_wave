87cfc497b62ba044ae3f10246c4a038d92d7b25c
r4if2gjhd97ws <tbskpercussion@gmail.com>
2022-02-06 20:35:13 +0900

    sinewave

--- sine_wave/arduino_sine_wave/src/main.cpp
+++ sine_wave/arduino_sine_wave/src/main.cpp
@@ -1,16 +1,30 @@
 #include <Arduino.h>
-#include <MozziGuts.h>
-#include <Oscil.h>
-#include <tables/sin2048_int8.h>
 
-Oscil<SIN2048_NUM_CELLS, AUDIO_RATE> asin(SIN2048_DATA);
+#include <MozziGuts.h>
+#include <Oscil.h>               // Oscillator template
+#include <tables/sin2048_int8.h> // sine table for oscillator
+
+Oscil<SIN2048_NUM_CELLS, AUDIO_RATE> aSin(SIN2048_DATA);
+
+// use #define for CONTROL_RATE, not a constant
+#define CONTROL_RATE 64 // Hz, power of 2 are most reliable
 
 void setup()
 {
-  // put your setup code here, to run once:
+  startMozzi(CONTROL_RATE);
+  aSin.setFreq(440);
+}
+
+void updateControl()
+{
+}
+
+AudioOutput_t updateAudio()
+{
+  return MonoOutput::from8Bit(aSin.next());
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  audioHook();
 }
