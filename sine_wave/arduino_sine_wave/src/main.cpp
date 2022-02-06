20a6b8618617ddfd5d25f1ca83473320cca595bf
yohei_takasaki <y.takasaki@speakbuddy.jp>
2022-02-06 19:31:52 +0900

    add Mozzi libraly

--- sine_wave/arduino_sine_wave/src/main.cpp
+++ sine_wave/arduino_sine_wave/src/main.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
+#include <MozziGuts.h>
+#include <Oscil.h>
+#include <tables/sin2048_int8.h>
 
-void setup() {
+Oscil<SIN2048_NUM_CELLS, AUDIO_RATE> asin(SIN2048_DATA);
+
+void setup()
+{
   // put your setup code here, to run once:
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
