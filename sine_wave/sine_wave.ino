6ef037d681898d4d254251e27377262b5040f52b
yohei_takasaki <y.takasaki@speakbuddy.jp>
2022-02-06 19:24:35 +0900

    platform io

--- sine_wave/sine_wave.ino
+++ sine_wave/sine_wave.ino
@@ -1,9 +1,26 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <MozziGuts.h>
+#include <Oscil.h>               // Oscillator template
+#include <tables/sin2048_int8.h> // sine table for oscillator
 
+Oscil<SIN2048_NUM_CELLS, AUDIO_RATE> aSin(SIN2048_DATA);
+
+// use #define for CONTROL_RATE, not a constant
+#define CONTROL_RATE 64 // Hz, powers of 2 are most reliable
+
+void setup()
+{
+  startMozzi(CONTROL_RATE);
+  aSin.setFreq(440);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void updateControl()
+{
+}
 
+AudioOutput_t updateAudio() {
+  return Mono
+}
+
+void loop()
+{
 }
