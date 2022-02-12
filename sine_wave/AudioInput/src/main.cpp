f2a341ccd9b754666c424679231d39fc148c3cef
r4if2gjhd97ws <tbskpercussion@gmail.com>
2022-02-13 00:13:39 +0900

    Mozziライブラリ追加

--- sine_wave/AudioInput/src/main.cpp
+++ sine_wave/AudioInput/src/main.cpp
@@ -1,9 +1,13 @@
+
+#include <MozziGuts.h>
 #include <Arduino.h>
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
